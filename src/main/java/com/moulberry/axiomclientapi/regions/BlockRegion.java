d2cc71a55c7c494c772b41f1a8178206019faf2c
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-06-15 02:01:30 +0000

    Update BlockRegion.java

--- src/main/java/com/moulberry/axiomclientapi/regions/BlockRegion.java
+++ src/main/java/com/moulberry/axiomclientapi/regions/BlockRegion.java
@@ -1,4 +1,17 @@
 package com.moulberry.axiomclientapi.regions;
 
+import com.moulberry.axiomclientapi.IAxiomWorldRenderContext;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+
 public interface BlockRegion {
+    void clear();
+    void render(IAxiomWorldRenderContext rc, Vec3 translation, float blockOpacity, float outlineOpacity);
+    void addBlockIfNotPresent(int x, int y, int z, BlockState block);
+    void addBlock(int x, int y, int z, BlockState block);
+    BlockPos min();
+    BlockPos max();
+    boolean isEmpty();
+    int count();
 }
